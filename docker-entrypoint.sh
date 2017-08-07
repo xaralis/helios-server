2d3ac0be7b9e7af338749a2bd7f07c124eff3dcd
Stanislav Stipl <stanislav.stipl@gmail.com>
2017-08-07 14:06:56 +0200

    remove db from container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,20 +2,11 @@
 
 trap "echo TRAPed signal" HUP INT QUIT KILL TERM
 
-chown postgres:postgres /var/lib/postgresql
-
 /etc/init.d/rabbitmq-server start
-/etc/init.d/postgresql start
-
-echo 'CREATE USER root;' | psql -U postgres
-echo 'CREATE DATABASE helios;' | psql -U postgres 2>&1 | grep -q "already exists"
-HASDB=$?
 
 source venv/bin/activate
 
-if [ ${HASDB} -ne 0 ]; then
-	python manage.py syncdb
-fi
+python manage.py syncdb
 
 python manage.py migrate
 
@@ -24,5 +15,4 @@ python manage.py celeryd &
 python manage.py runserver 0.0.0.0:80
 
 /etc/init.d/rabbitmq-server stop
-/etc/init.d/postgresql stop
 
